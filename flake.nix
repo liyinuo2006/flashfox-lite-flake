d319d0a10555b8232c6054754439d945299bea25
liyinuo2006 <orionli2006@gmail.com>
2026-08-04 19:17:19 +0800

    模块改为选项风格: programs.flashfox-lite.enable / package

--- flake.nix
+++ flake.nix
@@ -25,15 +25,32 @@
         flashfox-lite = final.callPackage ./package.nix { };
       };
 
-      # NixOS 模块:装包 + 装 gsettings schema。
+      # NixOS 模块:提供 programs.flashfox-lite 选项,enable 后装包 + gsettings schema。
       # 闪狐用 gsettings 设置系统代理(org.gnome.system.proxy),
       # NixOS 默认缺该 schema,不装则代理设置静默失败,浏览器不走代理。
-      nixosModules.default = { pkgs, ... }: {
-        nixpkgs.overlays = [ self.overlays.default ];
-        environment.systemPackages = [
-          pkgs.flashfox-lite
-          pkgs.gsettings-desktop-schemas
-        ];
-      };
+      nixosModules.default =
+        {
+          lib,
+          pkgs,
+          config,
+          ...
+        }:
+        let
+          cfg = config.programs.flashfox-lite;
+        in
+        {
+          options.programs.flashfox-lite = {
+            enable = lib.mkEnableOption "闪狐云 Lite";
+            package = lib.mkPackageOption pkgs "flashfox-lite" { };
+          };
+
+          config = lib.mkIf cfg.enable {
+            nixpkgs.overlays = [ self.overlays.default ];
+            environment.systemPackages = [
+              cfg.package
+              pkgs.gsettings-desktop-schemas
+            ];
+          };
+        };
     };
 }
